40fdc1b05e294a20cba6e5bc32e4f9451921979b
Jacek <jacek@kuzemczak.co.uk>
2012-06-02 18:37:52 +0100

    We dont need this now :)

--- uk/co/jacekk/bukkit/automod/command/VoteExecutor.java
+++ uk/co/jacekk/bukkit/automod/command/VoteExecutor.java
@@ -3,7 +3,6 @@ package uk.co.jacekk.bukkit.automod.command;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import uk.co.jacekk.bukkit.automod.AutoMod;
 import uk.co.jacekk.bukkit.automod.Permission;
