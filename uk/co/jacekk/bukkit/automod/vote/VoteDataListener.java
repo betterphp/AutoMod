17e8b2950667c9cff2c67d6f459a60b1630d785d
Jacek <jacek@kuzemczak.co.uk>
2012-06-02 18:36:11 +0100

    forgot this file

---
+++ uk/co/jacekk/bukkit/automod/vote/VoteDataListener.java
@@ -0,0 +1,38 @@
+package uk.co.jacekk.bukkit.automod.vote;
+
+import java.util.Map.Entry;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import uk.co.jacekk.bukkit.automod.AutoMod;
+import uk.co.jacekk.bukkit.automod.Permission;
+import uk.co.jacekk.bukkit.baseplugin.BaseListener;
+
+public class VoteDataListener extends BaseListener<AutoMod> {
+	
+	public VoteDataListener(AutoMod plugin){
+		super(plugin);
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onPlayerQuit(PlayerQuitEvent event){
+		Player player = event.getPlayer();
+		
+		if (Permission.ADMIN_VOTE.hasPermission(player)){
+			String playerName = player.getName();
+			
+			for (Entry<String, VoteData> entry : plugin.voteDataManager.getAll()){
+				VoteData voteData = entry.getValue();
+				
+				if (!voteData.voted.contains(playerName)){
+					--voteData.totalNeeded;
+					plugin.processVoteData(entry.getKey(), voteData);
+				}
+			}
+		}
+	}
+	
+}
